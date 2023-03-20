4206849602acc78a3435ec5e99e18b6dd05e2b9f
Javiera Fortunato <javiera.fortunato@perficient.com>
2023-03-20 16:35:46 -0300

    Testing button initial state and changes on click

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("button has correct initial text and color", () => {
   render(<App />);
-  const linkElement = screen.getByRole("link", { name: /learn react/i });
-  expect(linkElement).toBeInTheDocument();
+  // find an element with a role of button and text of 'Change to blue'
+  const buttonElement = screen.getByRole("button", { name: "Change to blue" });
+  // expect the background color to be red
+  expect(buttonElement).toHaveStyle({ backgroundColor: "red" });
+});
+
+test("button turns blue when clicked and has the correct text", () => {
+  render(<App />);
+  const buttonElement = screen.getByRole("button", { name: "Change to blue" });
+  // click button
+  fireEvent.click(buttonElement);
+  // expect the background color to be blue
+  expect(buttonElement).toHaveStyle({ backgroundColor: "blue" });
+  // expect the button text to be 'Change to red'
+  expect(buttonElement).toHaveTextContent("Change to red");
 });
